e0a7a30951efe93cebe84de039fcc9f9f5a34491
GeoFernandes <geovannaferoliveira@gmail.com>
2024-09-18 20:37:12 -0300

    Ajuste no funcioamento dos enpoints de pegar usuario por id e excluir

--- src/controllers/usuario/usuarioController.ts
+++ src/controllers/usuario/usuarioController.ts
@@ -1,5 +1,4 @@
-import { Post, Body, HttpCode, Controller, Res, Get, Params, UseBefore, Delete } from 'routing-controllers';
-import { Response } from 'express';
+import { Post, Body, HttpCode, Controller, Res, Get, Params, UseBefore, Delete, JsonController } from 'routing-controllers';
 import { ILogin, IUsuario } from '../../shared/interface';
 import UsuarioService from '../../services/usuario.service';
 import { Usuario } from '../../schemas/usuario-schema';
@@ -8,12 +7,14 @@ import { verificaToken } from '../../autenticacao/auth.middleware';
 import { OpenAPI } from 'routing-controllers-openapi';
 
 @Controller()
+@JsonController()
 class UsuarioController {
 
   @Post("/auth/register")
   @HttpCode(201)
   @OpenAPI({ summary: 'Registra um novo usuário', description: 'Cria um novo usuário com os dados fornecidos' })
   async registroDeUsuario(@Body() dadoUsuario: IUsuario): Promise<{ msg: string, objeto?: any }> {
+    console.log(dadoUsuario);
     try {
       const dadosValidos = await UsuarioService.validaDadosUsuario(dadoUsuario);
       if (!dadosValidos) return { msg: "Preencha todos os campos obrigatórios corretamente." };
@@ -34,6 +35,7 @@ class UsuarioController {
   @HttpCode(200)
   @OpenAPI({ summary: 'Faz login do usuário', description: 'Autentica o usuário com email e senha' })
   async loginDoUsuario(@Body() dadosLogin: ILogin): Promise<{ msg: string, token?: string }> {
+    console.log(dadosLogin);
     try {
       const resultado = await UsuarioService.verificaDadosLogin(dadosLogin);
       return resultado;
@@ -46,34 +48,42 @@ class UsuarioController {
   @Get("/user/:id")
   @UseBefore(verificaToken)
   @OpenAPI({ summary: 'Obtém informações do usuário', description: 'Busca as informações de um usuário pelo ID' })
-  async informacoesUsuario(@Params() idUsuario: string): Promise<{ msg: string, usuario?: any }> {
-    if (!mongoose.Types.ObjectId.isValid(idUsuario)) {
+  async informacoesUsuario(@Params() params: { id: string }): Promise<{ msg: string, usuario?: any }> {
+    const { id } = params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
       return { msg: "ID inválido." };
     }
 
     try {
-      const usuario = await Usuario.findById(idUsuario, '-senha');
+      const usuario = await Usuario.findById(id, '-senha').lean();
+
       if (!usuario) {
         return { msg: "Usuário não encontrado." };
       }
+
       return { msg: "Usuário encontrado.", usuario };
     } catch (erro) {
+      console.error("Erro ao buscar o usuário:", erro);
       return { msg: "Erro ao buscar o usuário." };
     }
   }
 
 
+
+
   @Delete("/user/:id")
   @UseBefore(verificaToken)
   @OpenAPI({ summary: 'Exlui informações do usuário', description: 'exclui as informações de um usuário pelo ID' })
-  async deletaUsuario(@Params() idUsuario: string): Promise<{ msg: string, usuario?: any }> {
-    idUsuario = '66e70b722c7188e72d582b91'
-    if (!mongoose.Types.ObjectId.isValid(idUsuario)) {
+  async deletaUsuario(@Params() params: { id: string }): Promise<{ msg: string, usuario?: any }> {
+    const { id } = params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
       return { msg: "ID inválido." };
     }
 
+    console.log(id)
     try {
-      const usuarioDeletado = await Usuario.deleteOne({id: idUsuario});
+      const usuarioDeletado = await Usuario.deleteOne({ _id: id});
       if (!usuarioDeletado) {
         return { msg: "Usuário não encontrado." };
       }
